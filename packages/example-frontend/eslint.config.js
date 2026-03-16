928e0ae52ccce61564ed6d4ad15f1a7e7bce2847
Shan8851.eth <asamshan456@gmail.com>
2026-03-16 12:44:30 +0000

    chore: migrate apps-team-lint to new version

---
+++ packages/example-frontend/eslint.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'eslint/config';
+
+import { frontend, recommended, typescript } from '@polygonlabs/apps-team-lint';
+
+export default defineConfig([
+  ...recommended({ globals: 'browser' }),
+  ...typescript({ tsconfigRootDir: import.meta.dirname }),
+  ...frontend()
+]);
